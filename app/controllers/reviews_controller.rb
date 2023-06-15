cac5fc3c59688212df73b541866e7ab39a99f28b
Yasser Bouyanfif <yasser.bouyanfif@gmail.com>
2023-06-15 11:32:49 +0200

    exo2g

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,13 @@
 class ReviewsController < ApplicationController
-
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    @review.save!
-    redirect_to restaurant_path(@restaurant)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'restaurants/new', status: :unprocessable_entity
+    end
   end
 
   private
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  validates :name, :address, :phone_number, presence: :true
   CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
   validates :category, inclusion: {in: CATEGORY}
   #  Restaurant::CATEORY
