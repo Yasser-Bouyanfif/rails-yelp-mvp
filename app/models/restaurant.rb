9df376d899d29b042b76adf91365c1c2f0b45b3a
Yasser Bouyanfif <yasser.bouyanfif@gmail.com>
2023-08-25 13:05:38 +0200

    rails new

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,5 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, :address, :phone_number, presence: :true
   CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
-  validates :category, inclusion: {in: CATEGORY}
-  #  Restaurant::CATEORY
+  validates :category, inclusion: { in: CATEGORY }
 end
